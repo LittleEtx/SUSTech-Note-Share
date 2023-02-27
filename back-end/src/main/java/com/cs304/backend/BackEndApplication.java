a248784aa9ee648d1ab026001bdd31c058b16015
LittleEtx <53439086+littleetx@users.noreply.github.com>
2023-02-27 16:50:32 +0800

    initial project

---
+++ back-end/src/main/java/com/cs304/backend/BackEndApplication.java
@@ -0,0 +1,17 @@
+package com.cs304.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+}
