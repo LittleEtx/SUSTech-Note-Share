bff2a2e6a594a741ddadf686a9f814335f806bff
acbgo <1105717400@qq.com>
2023-03-08 16:54:53 +0800

    update user interface

--- src/main/java/com/example/SUSTechNote/app/UserApp.java
+++ src/main/java/com/example/SUSTechNote/app/UserApp.java
@@ -1,12 +1,14 @@
 package com.example.SUSTechNote.app;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.SUSTechNote.entity.User;
 import com.example.SUSTechNote.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -15,21 +17,35 @@ public class UserApp {
     @Autowired
     UserService userService;
 
-    @GetMapping("/login")
-    public int login(int userID, String password) {
+    @PostMapping("/login")
+    public int login(@RequestBody JSONObject jsonpObject) {
+        int userID = Integer.parseInt(jsonpObject.getString("userID"));
+        String password = jsonpObject.getString("password");
         return userService.login(userID, password);
     }
     @PostMapping("/register")
-    public int register(int userID, String password, String email, String userName){
+    public int register(@RequestBody JSONObject jsonpObject){
+        int userID = Integer.parseInt(jsonpObject.getString("userID"));
+        String password = jsonpObject.getString("password");
+        String email = jsonpObject.getString("email");
+        String userName = jsonpObject.getString("userName");
         return userService.register(userID,password,email,userName);
     }
     @PostMapping("/updateUser")
-    public int register(User user){
+    public int update(@RequestBody JSONObject jsonpObject){
+        User user = new User();
+        user.setUserID(Integer.parseInt(jsonpObject.getString("userID")));
+        user.setUserName(jsonpObject.getString("userName"));
+        user.setPassword(jsonpObject.getString("password"));
+        user.setEmail(jsonpObject.getString("email"));
+        user.setSchedule(jsonpObject.getString("schedule"));
+        user.setAvatar(jsonpObject.getString("avatar"));
         return userService.updateUser(user);
     }
 
-    @GetMapping("/deleteUser")
-    public int deleteUser(int userID){
+    @PostMapping("/deleteUser")
+    public int deleteUser(@RequestBody JSONObject jsonpObject){
+        int userID = Integer.parseInt(jsonpObject.getString("userID"));
         return userService.deleteUser(userID);
     }
 
