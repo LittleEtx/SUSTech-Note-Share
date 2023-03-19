65140016e5657561e00904af142514307d2a4f57
acbgo <1105717400@qq.com>
2023-03-19 22:30:25 +0800

    完成上传用户头像接口

--- back-end/src/main/java/com/example/SUSTechNote/app/UserDetail.java
+++ back-end/src/main/java/com/example/SUSTechNote/app/UserDetail.java
@@ -6,7 +6,9 @@ import com.example.SUSTechNote.entity.User;
 import com.example.SUSTechNote.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 @RestController
@@ -25,6 +27,34 @@ public class UserDetail {
         user.setSchedule(jsonObject.getString("schedule"));
         user.setAvatar(jsonObject.getString("avatar"));
         return userService.updateUser(user);
+    }
+
+    @PostMapping("/upload-avatar")
+    public int uploadAvatar(@RequestParam("avatar") MultipartFile[] files) {
+        System.out.println("The number of files received: " + files.length);
+        for (MultipartFile file : files) {
+            String fileType = file.getContentType();
+            System.out.println(fileType);
+            if (!(fileType.equals("image/jpeg") || fileType.equals("image/png"))) {
+                return 406;
+            }
+            String fileName = file.getOriginalFilename();
+            System.out.println("Saving " + fileName);
+            String savingPath = "E:\\SoftwareEngineering\\UserAvatar";
+            File folder = new File(savingPath);
+            if (!folder.isDirectory()) {
+                folder.mkdirs();
+            }
+            try {
+                file.transferTo(new File(folder, fileName));
+                String filePath = savingPath + "\\" + fileName;
+                System.out.println(filePath);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return 500;
+            }
+        }
+        return 200;
     }
 
     @SaCheckRole("admin")
