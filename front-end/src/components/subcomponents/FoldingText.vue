832586d4abda0d0e8ea885d0943e0eae9d1daaef
little_etx <1968717506@qq.com>
2023-04-20 01:09:40 +0800

    add FoldingText.vue and NotebookPage.vue

---
+++ front-end/src/components/subcomponents/FoldingText.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'FoldingText',
+  props: {
+    text: {
+      type: String,
+      require: true
+    },
+    defaultHeight: {
+      type: Number,
+      default: 200
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
