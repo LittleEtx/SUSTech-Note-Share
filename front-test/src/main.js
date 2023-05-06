9e32b2b0cd27a992e33fe2247a6c7f7c169414ac
little_etx <1968717506@qq.com>
2023-05-07 00:41:33 +0800

    modify global color via scss

--- front-test/src/main.js
+++ front-test/src/main.js
@@ -3,7 +3,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import '@/assets/element-variables.scss'
 import '@/assets/globle.css'
 import '@/scripts/AxiosSettings.js'
 import {router} from "@/router"
