8562ef1b8ce84161c8d77056b68d6e137a0ca0b9
little_etx <1968717506@qq.com>
2023-05-06 18:06:03 +0800

    config axios

--- front-test/vite.config.js
+++ front-test/vite.config.js
@@ -13,13 +13,12 @@ export default defineConfig({
   },
   server: {
     port: 8080,
+    open: true, // 在服务器启动时自动在浏览器中打开应用程序
     proxy: {
-      '/api':{
+      '^/api':{
         target: 'http://localhost:8088/api/',      //后端接口的根目录
         changeOrigin: true,                    //是否跨域
-        pathRewrite: {
-          '^/api': ''
-        }
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
