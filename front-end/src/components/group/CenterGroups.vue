034eb22f967cc2f197f3562915e4fc2632c4d974
hyh66666 <12011507@mail.sustech.edu.cn>
2023-05-13 18:36:04 +0800

    完成群组主页面的后端对接

--- front-end/src/components/group/CenterGroups.vue
+++ front-end/src/components/group/CenterGroups.vue
@@ -3,30 +3,19 @@
     <el-card class="note-card create-card" shadow="hover" @click="handleClickCreateCard">
       <div class="create-card-icon"><el-icon><Plus /></el-icon></div>
     </el-card>
-    <el-card v-for="note in group" :key="note.id" class="note-card" shadow="hover" @click="handleClickNoteCard(note, $event)">
+    <el-card v-for="note in group" :key="note.groupID" class="note-card" shadow="hover" @click="handleClickNoteCard(note, $event)">
       <template #header>
         <div class="note-title note-header">
           <el-icon><Notebook /></el-icon>
-          <span style="margin-left: 5px">{{ note.title }}</span></div>
-        <div class="note-date">{{ note.date }}</div>
+          <span style="margin-left: 5px">{{ note.groupName }}</span></div>
+        <div class="note-date">{{ note.createTime }}</div>
       </template>
-      <div class="note-content">描述信息：{{ note.content }}</div>
-      <div class="note-creator">群主：{{ note.creator }}</div>
+      <div class="note-content">描述信息：{{ note.groupDescription }}</div>
+      <div class="note-creator">群主：{{ note.groupOwnerName }}</div>
       <div class="note-footer">
-        <el-button v-if="nowUser === note.creator" type="success" @click.stop="edit(note)">编辑</el-button>
-<!--        <el-popconfirm-->
-<!--            confirm-button-text="是"-->
-<!--            cancel-button-text="否"-->
-<!--            icon-color="#626AEF"-->
-<!--            title="请确认是否要解散/退出该群组？"-->
-<!--            @confirm="remove(note.id)"-->
-<!--            @cancel="cancelEvent"-->
-<!--        >-->
-<!--          <template #reference>-->
-<!--            <el-button type="danger" round>删除</el-button>-->
-<!--          </template>-->
-<!--        </el-popconfirm>-->
-        <el-button type="danger" @click.stop="remove(note.id)">删除</el-button>
+        <el-button v-if="nowUser === note.groupOwnerID" type="success" @click.stop="edit(note)">编辑</el-button>
+        <el-button v-if="nowUser === note.groupOwnerID" type="danger" @click.stop="disband(note.groupID)">解散</el-button>
+        <el-button v-else type="danger" @click.stop="remove(note.groupID)">退出</el-button>
       </div>
     </el-card>
     <el-dialog
@@ -42,13 +31,13 @@
         <template v-if="activeName === 'creat'">
           <!--   创建群组表单     -->
           <el-form ref="form" :model="form" label-width="80px" :rules="rules">
-            <el-form-item label="组名" prop="title">
-              <el-input v-model="form.title"
+            <el-form-item label="组名" prop="groupName">
+              <el-input v-model="form.groupName"
                         placeholder="请输入群组名字"
                         onkeyup="this.value=this.value.replace(/\s+/g,'')"></el-input>
             </el-form-item>
-            <el-form-item label="描述信息" prop="content">
-              <el-input v-model="form.content" placeholder="请输入描述信息"></el-input>
+            <el-form-item label="描述信息" prop="groupDescription">
+              <el-input v-model="form.groupDescription" placeholder="请输入描述信息"></el-input>
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="createNote('form')">创建</el-button>
@@ -61,10 +50,10 @@
           <el-form ref="ruleForm" :model="ruleForm" label-width="120px" :rules="join_rules">
             <el-form-item label="选择加入的群组" prop="groupJoin">
               <el-select  v-model="ruleForm.groupJoin" placeholder="请选择群组">
-                <el-option v-for="(item, index) in groupList"  :key="index" :label="item.name" :value="item.name" />
+                <el-option v-for="(item, index) in groupList"  :key="index" :label="item.groupName" :value="item.groupID" />
               </el-select>
             </el-form-item>
-            <div v-if="selectedGroup" style="margin-top: 10px; color: #999">描述信息：{{ selectedGroup.description }}</div>
+            <div v-if="selectedGroup" style="margin-top: 10px; color: #999">描述信息：{{ selectedGroup.groupDescription }}</div>
             <el-form-item>
               <el-button style="margin-top: 15px" type="primary" @click="joinGroup('ruleForm')">加入</el-button>
               <el-button style="margin-top: 15px" @click="cancelForm()">取消</el-button>
@@ -79,16 +68,16 @@
         width="500px"
     >
       <el-form ref="form" :model="form" label-width="80px" :rules="rules">
-        <el-form-item label="组名" prop="title">
-          <el-input v-model="form.title"
+        <el-form-item label="组名" prop="groupName">
+          <el-input v-model="form.groupName"
                     placeholder="请输入群组名字"
                     onkeyup="this.value=this.value.replace(/\s+/g,'')"></el-input>
         </el-form-item>
-        <el-form-item label="描述信息" prop="content">
-          <el-input v-model="form.content" placeholder="请输入描述信息"></el-input>
+        <el-form-item label="描述信息" prop="groupDescription">
+          <el-input v-model="form.groupDescription" placeholder="请输入描述信息"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="createNote('form')">创建</el-button>
+          <el-button type="primary" @click="editNote('form')">编辑</el-button>
           <el-button @click="resetForm()">重置</el-button>
         </el-form-item>
       </el-form>
@@ -98,63 +87,76 @@
 
 <script>
 import { Notebook, Plus } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'
+import axios from 'axios'
 // 打开页面时从后端读取存在的群组并做过滤，然后保存到groupList中；
 // 读取当前用户加入的群组(群组名称、描述信息、创建日期)
 export default {
   components: { Notebook, Plus },
+  mounted () {
+    this.getData()
+    setInterval(() => {
+      this.getData()
+    }, 100)
+  },
   data () {
     return {
-      nowUser: '杨子德',
+      nowUser: 12112628,
       group: [
         {
-          id: 1,
-          title: '高数',
-          date: '2022-05-07',
-          content: '这是该群组的描述',
-          creator: '黄宇海'
+          groupID: 1,
+          groupName: '高数',
+          createTime: '2022-05-07',
+          groupDescription: '这是该群组的描述',
+          groupOwnerID: 12112628,
+          groupOwnerName: '黄宇海'
         },
         {
-          id: 2,
-          title: '大物',
-          date: '2022-05-06',
-          content: '这是该群组的描述',
-          creator: '杨子德'
+          groupID: 2,
+          groupName: '大物',
+          createTime: '2022-05-06',
+          groupDescription: '这是该群组的描述',
+          groupOwnerID: 12112628,
+          groupOwnerName: '杨子德'
         },
         {
-          id: 3,
-          title: '计网',
-          date: '2022-05-05',
-          content: '',
-          creator: '陈孙兵'
+          groupID: 3,
+          groupName: '计网',
+          createTime: '2022-05-05',
+          groupDescription: '',
+          groupOwnerID: 12112628,
+          groupOwnerName: '陈孙兵'
         },
         {
-          id: 4,
-          title: '计网',
-          date: '2022-05-05',
-          content: '这是该群组的描述',
-          creator: '陈孙兵'
+          groupID: 4,
+          groupName: '计网',
+          createTime: '2022-05-05',
+          groupDescription: '这是该群组的描述',
+          groupOwnerID: 12112628,
+          groupOwnerName: '陈孙兵'
         }
       ],
       activeName: 'creat',
       createFormVisible: false,
       editFormVisible: false,
       form: {
-        id: 1,
-        title: '',
-        date: '',
-        content: '',
-        creator: '黄宇海'
+        groupID: 1,
+        groupName: '',
+        createTime: '',
+        groupDescription: '',
+        groupOwnerID: 12112628,
+        groupOwnerName: '李田'
       },
       rules: {
-        title: [
+        groupName: [
           { required: true, message: '群组名称不能为空', trigger: 'blur' }
         ]
       },
       groupList: [
-        { name: '高数', description: '这是高数的描述' },
-        { name: '大物', description: '这是大物的描述' },
-        { name: '计网', description: '这是计网的描述' },
-        { name: '离散', description: '这是离散的描述' }],
+        {groupID: '', groupName: '高数', groupDescription: '这是高数的描述' },
+        {groupID: '', groupName: '大物', groupDescription: '这是大物的描述' },
+        {groupID: '', groupName: '计网', groupDescription: '这是计网的描述' },
+        {groupID: '', groupName: '离散', groupDescription: '这是离散的描述' }],
       ruleForm: {
         groupJoin: ''
       },
@@ -167,18 +169,56 @@ export default {
   },
   computed: {
     selectedGroup() {
-      return this.groupList.find(group => group.name === this.ruleForm.groupJoin)
+      return this.groupList.find(group => group.groupID === this.ruleForm.groupJoin)
     }
   },
   methods: {
+    getData(){
+      axios.get("http://10.32.58.153:8088/api/group/loadJoinedGroup").then(res => {
+        this.group = res.data;
+      });
+      axios.get("http://10.32.58.153:8088/api/group/loadEnjoinedGroup").then(res => {
+        this.groupList = res.data;
+      });
+    },
     createNote (formName) {
-      // 将当前用户（群主）、群组名称、群组描述、创建日期发给后端
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          // alert('submit!');
           const d = new Date()
-          const date = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
-          alert(date)
+          this.form.createTime = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
+          axios.post("http://10.32.58.153:8088/api/group/createGroup",{
+            groupName: this.form.groupName,
+            groupDescription: this.form.groupDescription,
+            createTime: this.form.createTime
+          }).then(res => {
+            ElMessage({
+              message: res.data,
+              type: 'success',
+            })
+          })
+          this.createFormVisible = false
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    editNote (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const d = new Date()
+          this.form.createTime = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate()
+          axios.post("http://10.32.58.153:8088/api/group/updateGroup",{
+            groupID: this.form.groupID,
+            groupName: this.form.groupName,
+            groupDescription: this.form.groupDescription
+          }).then(res => {
+            ElMessage({
+              message: res.data,
+              type: 'success',
+            })
+          })
+          this.editFormVisible = false
         } else {
           console.log('error submit!!')
           return false
@@ -189,7 +229,15 @@ export default {
       // 直接加入群组
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          alert('submit!')
+          axios.post("http://10.32.58.153:8088/api/group/joinGroup",{
+            groupID: this.ruleForm.groupJoin
+          }).then(res => {
+            ElMessage({
+              message: res.data,
+              type: 'success',
+            })
+          })
+          this.createFormVisible = false
         } else {
           console.log('error submit!!')
           return false
@@ -197,34 +245,44 @@ export default {
       })
     },
     resetForm () {
-      this.form.title = ''
-      this.form.content = ''
+      this.form.groupName = ''
+      this.form.groupDescription = ''
     },
     cancelForm () {
-      this.form.title = ''
-      this.form.content = ''
+      this.form.groupName = ''
+      this.form.groupDescription = ''
       this.createFormVisible = false
     },
     edit (note) {
-      // 修改群信息(名称、描述信息)
       this.form = Object.assign({}, note)
       this.editFormVisible = true
     },
+    // 退出群组
     remove (id) {
-      // 解散或者退出群组
-      this.group = this.group.filter(note => note.id !== id)
+      axios.post("http://10.32.58.153:8088/api/group/quitGroup",{
+        groupID: id
+      }).then(res => {
+        ElMessage.error(res.data)
+      })
+    },
+    //解散群组
+    disband(id) {
+      axios.post("http://10.32.58.153:8088/api/group/deleteGroup",{
+        groupID: id
+      }).then(res => {
+        ElMessage.error(res.data)
+      })
     },
     handleClickNoteCard (note, event) {
-      // 处理点击卡片事件
       if (event.target.tagName === 'BUTTON') {
         return
       }
       this.$router.push('/showTest')
     },
     handleClickCreateCard () {
-      // 处理创建新卡片的逻辑
-      this.form.title = ''
-      this.form.content = ''
+      this.form.groupName = ''
+      this.form.groupDescription = ''
+      this.ruleForm.groupJoin = ''
       this.createFormVisible = true
     }
   }
@@ -239,14 +297,9 @@ export default {
 }
 
 .note-card {
-  width: 320px;
-  height: 320px;
+  width: 270px;
+  height: 250px;
   position: relative;
-  /*background-image: url('../../assets/icon/icon_with_words_shadow.svg');*/
-  /*background-repeat: no-repeat;*/
-  /*background-size: cover;*/
-  /*background-position: center;*/
-  /*opacity: 0.4; !* 控制背景图片的透明度 *!*/
   background-color: #f2f2f2;
 }
 
