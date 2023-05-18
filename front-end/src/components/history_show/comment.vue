dabea774e3311f1384a6541a7863d97016604a5d
hyh66666 <12011507@mail.sustech.edu.cn>
2023-05-18 22:36:33 +0800

    评论区框架，以及相关依赖

--- front-end/src/components/history_show/comment.vue
+++ front-end/src/components/history_show/comment.vue
@@ -1,149 +1,188 @@
 <template>
   <div>
-    <h2>评论区</h2>
-    <form @submit.prevent="addComment" class="comment-form">
-      <!-- 表单内容 -->
-      <div class="form-group">
-        <label for="author">作者</label>
-        <input type="text" id="author" v-model="newComment.author" required>
+    <div v-for="(item,i) in comments" :key="i" class="author-title reply-father">
+      <el-avatar class="header-img" :size="40" :src="item.headImg"></el-avatar>
+      <div class="author-info">
+        <span class="author-name">{{item.name}}</span>
+        <span class="author-time">{{item.time}}</span>
       </div>
-      <div class="form-group">
-        <label for="comment">评论</label>
-        <textarea id="comment" v-model="newComment.text" required></textarea>
+      <div class="icon-btn">
+        <span><el-icon><ChatDotSquare /></el-icon>{{item.commentNum}}</span>
+        <el-icon><Star /></el-icon>{{item.like}}
       </div>
-      <div class="form-group">
-        <label for="avatar">头像</label>
-        <input type="text" id="avatar" v-model="newComment.avatar" placeholder="https://example.com/avatar.png">
+      <div class="talk-box">
+        <p>
+          <span class="reply">{{item.comment}}</span>
+        </p>
       </div>
-      <button type="submit">提交评论</button>
-    </form>
-
-    <ul>
-      <li v-for="comment in comments" :key="comment.id" class="comment">
-
-        <div class="comment-header">
-          <!-- 评论者头像和名称 -->
-          <div class="avatar">
-            <img :src="comment.avatar" alt="用户头像">
+      <div class="reply-box">
+        <div v-for="(reply,j) in item.reply" :key="j" class="author-title">
+          <el-avatar class="header-img" :size="40" :src="reply.fromHeadImg"></el-avatar>
+          <div class="author-info">
+            <span class="author-name">{{reply.from}}</span>
+            <span class="author-time">{{reply.time}}</span>
+          </div>
+          <div class="icon-btn">
+            <span><el-icon><ChatDotSquare /></el-icon>{{reply.commentNum}}</span>
+            <span><el-icon><Star /></el-icon>{{reply.like}}</span>
+          </div>
+          <div class="talk-box">
+            <p>
+              <span>回复 {{reply.to}}:</span>
+              <span class="reply">{{reply.comment}}</span>
+            </p>
           </div>
         </div>
-
-        <div class="author">{{ comment.author }}</div>
-
-        <div class="comment-content">
-          <!-- 评论内容 -->
-            <div class="text">{{ comment.text }}</div>
-        </div>
-
-        <div v-if="index < comments.length - 1" class="comment-separator"></div>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-
-
 <script>
+import { ChatDotSquare, Star} from '@element-plus/icons-vue'
+
 export default {
-  name: "comment",
-  props:{
-    notebookId: {
-      required: true,
-      type: String
+  components: { ChatDotSquare, Star },
+  name:'ArticleComment',
+  data(){
+    return{
+      btnShow: false,
+      index:'0',
+      replyComment:'',
+      myName:'Lana Del Rey',
+      myHeader:'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+      myId:19870621,
+      to:'',
+      toId:-1,
+      comments:[
+        {
+          name:'Lana Del Rey',
+          id:19870621,
+          headImg:'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+          comment:'我发布一张新专辑Norman Fucking Rockwell,大家快来听啊',
+          time:'2019年9月16日 18:43',
+          commentNum:2,
+          like:15,
+          inputShow:false,
+          reply:[
+            {
+              from:'Taylor Swift',
+              fromId:19891221,
+              fromHeadImg:'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+              to:'Lana Del Rey',
+              toId:19870621,
+              comment:'我很喜欢你的新专辑！！',
+              time:'2019年9月16日 18:43',
+              commentNum:1,
+              like:15,
+              inputShow:false
+            },
+            {
+              from:'Ariana Grande',
+              fromId:1123,
+              fromHeadImg:'https://ae01.alicdn.com/kf/Hf6c0b4a7428b4edf866a9fbab75568e6U.jpg',
+              to:'Lana Del Rey',
+              toId:19870621,
+              comment:'别忘记宣传我们的合作单曲啊',
+              time:'2019年9月16日 18:43',
+              commentNum:0,
+              like:5,
+              inputShow:false
+
+            }
+          ]
+        }
+      ]
     }
   },
-  data() {
-    return {
-      comments: [
-        { id: 1, author: '小明', text: '这是第一条评论', avatar: 'https://randomuser.me/api/portraits/men/1.jpg' },
-        { id: 2, author: '小红', text: '这是第二条评论', avatar: 'https://randomuser.me/api/portraits/women/1.jpg' },
-      ],
-      newComment: {
-        author: '',
-        text: '',
-        avatar: '',
-      },
-    };
-  },
   methods: {
-    addComment() {
-      const newId = this.comments.length + 1;
-      this.comments.push({ ...this.newComment, id: newId });
-      this.newComment.author = '';
-      this.newComment.text = '';
-      this.newComment.avatar = '';
-    },
+
   },
-};
+}
 </script>
 
-<style scoped>
-.comment {
-  display: flex;
-  margin-bottom: 20px;
-}
-
-.comment-separator {
-  border: none;
-  border-top: 1px dashed #ccc;
-  margin: 10px 0;
-}
-
-.avatar {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-right: 10px;
-}
-
-.avatar img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.comment-content {
-  flex: 1;
-}
-
-.author {
-  font-weight: bold;
-  margin-bottom: 5px;
-}
-
-.text {
-  white-space: pre-wrap;
-}
-
-.comment-form {
-  margin-top: 30px;
-}
-
-.form-group {
-  margin-bottom: 10px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input[type="text"],
-textarea {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-sizing: border-box;
-}
-
-button[type="submit"] {
-  background-color: #007aff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  cursor: pointer;
-}
+<style lang="stylus" scoped>
+.my-reply
+  padding 10px
+  background-color #fafbfc
+  .header-img
+    display inline-block
+    vertical-align top
+  .reply-info
+    display inline-block
+    margin-left 5px
+    width 90%
+    @media screen and (max-width:1200px) {
+      width 80%
+    }
+    .reply-input
+      min-height 20px
+      line-height 22px
+      padding 10px 10px
+      color #ccc
+      background-color #fff
+      border-radius 5px
+      &:empty:before
+        content attr(placeholder)
+      &:focus:before
+        content none
+      &:focus
+        padding 8px 8px
+        border 2px solid blue
+        box-shadow none
+        outline none
+  .reply-btn-box
+    height 25px
+    margin 10px 0
+    .reply-btn
+      position relative
+      float right
+      margin-right 15px
+.my-comment-reply
+  margin-left 50px
+  .reply-input
+    width flex
+.author-title:not(:last-child)
+  border-bottom: 1px solid rgba(178,186,194,.3)
+.author-title
+  padding 10px
+  .header-img
+    display inline-block
+    vertical-align top
+  .author-info
+    display inline-block
+    margin-left 5px
+    width 60%
+    height 40px
+    line-height 20px
+    >span
+      display block
+      overflow hidden
+      white-space nowrap
+      text-overflow ellipsis
+    .author-name
+      color #000
+      font-size 18px
+      font-weight bold
+    .author-time
+      font-size 14px
+  .icon-btn
+    width 30%
+    padding 0 !important
+    float right
+    @media screen and (max-width : 1200px){
+      width 20%
+      padding 7px
+    }
+  .talk-box
+    margin 0 50px
+    >p
+      margin 0
+    .reply
+      font-size 16px
+      color #000
+  .reply-box
+    margin 10px 0 0 50px
+    background-color #efefef
 </style>
+
